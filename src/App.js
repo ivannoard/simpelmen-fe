97f3c82e37af5b20ddb2551c37b6da7554c11b49
roziqinkhoeru <roziqinkhoeru8@gmail.com>
2022-09-07 11:11:27 +0700

    config style environment

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold">
-        Hello world!
-      </h1>
+    <div className='App'>
+      <h1 className='text-3xl font-bold'>Hello world!</h1>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        success: "#21B630",
+        warning: "#FFBC0D",
+        dark: "#352A2B",
+        secondary: {
+          900: "#6D6061",
+          800: "#A69798",
+          700: "#E2DCDC",
+          600: "#F5F5F5",
+        },
+        primary: {
+          900: "#C9111B",
+          600: "#D64D54",
+          400: "#E4888D",
+        },
+        orange: {
+          900: "#ED7801",
+          600: "#F8CCA0",
+        },
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      spacing: {
+        "30/sp": "1.875rem",
+      },
+      fontSize: {
+        "45/sp": "45px",
+      },
+    },
   },
   plugins: [],
-}
+};

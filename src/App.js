3df5516e1bba790ff7f1378648fbbd3c3a510982
ivan <ivannovaa.24@gmail.com>
2022-09-07 12:00:54 +0700

    config routing & files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { Navigate, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Auth/Login";
+import Dashboard from "./pages/Dashboard";
+import Register from "./pages/Auth/Register";
+import WithoutFrame from "./components/global/WithoutFrame";
+import WithFrame from "./components/global/WithFrame";
 
 function App() {
+  const user = localStorage.getItem('user')
+  const RequireAuth = ({ children }) => {
+    return user !== null ? children : <Navigate to='/login' />
+  }
   return (
     <div className='App'>
-      <h1 className='text-3xl font-bold'>Hello world!</h1>
+      <Routes>
+        <Route element={<WithoutFrame />}>
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='*' element='404 Not Found' />
+        </Route>
+        <Route element={<WithFrame />}>
+          <Route path='/' element={<Home />} />
+          <Route path='/dashboard' element={<RequireAuth><Dashboard /></RequireAuth>} />
+        </Route>
+      </Routes>
     </div>
   );
 }

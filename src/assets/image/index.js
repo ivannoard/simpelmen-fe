9d98abe01e3a4e072af8678aa503f8e4a24b10dc
roziqinkhoeru <roziqinkhoeru8@gmail.com>
2022-09-11 12:55:55 +0700

    update style and responsiveness produk-kemasan page

--- src/assets/image/index.js
+++ src/assets/image/index.js
@@ -1,11 +1,15 @@
 import bentukSegitiga from "./dummy/bentuk-segitiga.png";
 import boxTentengan from "./dummy/box-tentengan.png";
+import boxModelPizza from "./dummy/box-model-pizza.png";
 import kotakBerdiri from "./dummy/kotak-berdiri.png";
 import topBottom from "./dummy/top-bottom.png";
+import piring from "./dummy/piring.png";
 
 export const dummyImg = {
   bentukSegitiga,
   boxTentengan,
+  boxModelPizza,
   kotakBerdiri,
   topBottom,
+  piring,
 };
--- src/pages/Kemasan/index.jsx
+++ src/pages/Kemasan/index.jsx
@@ -1,68 +1,110 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import CardProduct from "../../components/Card/CardProduct";
 
 import { dummyImg } from "../../assets/image";
 import { HiOutlineArrowSmLeft } from "react-icons/hi";
-import CardProduct from "../../components/Card/CardProduct";
+import { HiChevronRight, HiChevronLeft } from "react-icons/hi";
 
 const Kemasan = () => {
   const dummyData = [
     {
+      id: 1,
       produkImg: dummyImg.kotakBerdiri,
       altImg: "Kotak Berdiri",
       kategori: "Dus Offset",
       jenis: "Kotak Berdiri",
     },
     {
+      id: 2,
       produkImg: dummyImg.boxTentengan,
       altImg: "Box Tentengan",
       kategori: "Karton",
       jenis: "Box Tentengan",
     },
     {
+      id: 3,
       produkImg: dummyImg.topBottom,
       altImg: "Top Bottom",
       kategori: "Dus Offset",
       jenis: "Top Bottom",
     },
     {
+      id: 4,
       produkImg: dummyImg.bentukSegitiga,
       altImg: "Bentuk Segitiga",
       kategori: "Dus Offset",
       jenis: "Bentuk Segitiga",
     },
+    {
+      id: 5,
+      produkImg: dummyImg.boxModelPizza,
+      altImg: "Box Model Pizza",
+      kategori: "Karton",
+      jenis: "Box Model Pizza",
+    },
   ];
   return (
     <>
       <main className='containers'>
-        <section id='categoryKemasan' className='mt-7 mb-60/sp'>
+        <section id='categoryKemasan' className='mt-7 mb-12 2xsm:mb-60/sp'>
           <div className='mb-5'>
             <Link to='/' className='flex items-center mb-3'>
               <HiOutlineArrowSmLeft className='text-2xl mr-3' />
               <span className='leading-10'>Kembali</span>
             </Link>
           </div>
-          <div className='flex items-center gap-x-4'>
-            <button className='button-gradient-sm'>Semua Kemasan</button>
-            <button className='button-white-sm'>Karton</button>
-            <button className='button-white-sm'>Dus Offset</button>
-            <button className='button-white-sm'>
+          <div className='flex items-center gap-2 xs:gap-3 md:gap-4 flex-wrap'>
+            <button className='button-gradient-sm !text-xs xs:!text-base'>
+              Semua Kemasan
+            </button>
+            <button className='button-white-sm !text-xs xs:!text-base'>
+              Karton
+            </button>
+            <button className='button-white-sm !text-xs xs:!text-base'>
+              Dus Offset
+            </button>
+            <button className='button-white-sm !text-xs xs:!text-base'>
               Sablon Plastik, Pouch, Dus
             </button>
-            <button className='button-white-sm'>Sticker</button>
-            <button className='button-white-sm'>Standing Pouch</button>
+            <button className='button-white-sm !text-xs xs:!text-base'>
+              Sticker
+            </button>
+            <button className='button-white-sm !text-xs xs:!text-base'>
+              Standing Pouch
+            </button>
           </div>
         </section>
-        <section id='kemasan' className='mb-11'>
-          <div className='w-full grid grid-cols-12 gap-x-8 gap-y-10'>
+        <section id='kemasan' className='mb-9'>
+          <div className='w-full grid grid-cols-8 md:grid-cols-12 gap-3 2xsm:gap-5 xl:gap-7 mb-60/sp'>
             {dummyData.map((item, index) => {
               return (
-                <div className='col-span-4' key={index}>
+                <div className='col-span-4 lg:col-span-3' key={index}>
                   <CardProduct {...item} />
                 </div>
               );
             })}
           </div>
+          <nav
+            className='flex justify-center items-center gap-x-[.375rem] py-2'
+            aria-label='pagination'
+          >
+            <button className='button-white-sm !shadow-none hover:!shadow-red !text-xs xs:!text-base !px-3'>
+              <HiChevronLeft className='!text-base xs:!text-xl' />
+            </button>
+            <button className='button-gradient-sm !text-xs xs:!text-base'>
+              1
+            </button>
+            <button className='button-white-sm !shadow-none hover:!shadow-red !text-xs xs:!text-base'>
+              2
+            </button>
+            <button className='button-white-sm !shadow-none hover:!shadow-red !text-xs xs:!text-base'>
+              3
+            </button>
+            <button className='button-white-sm !shadow-none hover:!shadow-red !text-xs xs:!text-base !px-3'>
+              <HiChevronRight className='!text-base xs:!text-xl' />
+            </button>
+          </nav>
         </section>
       </main>
     </>

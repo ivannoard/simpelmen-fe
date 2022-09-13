f1de89e828c380a6bda8a8d6cb776173b5221370
ivan <ivannovaa.24@gmail.com>
2022-09-13 16:11:57 +0700

    fix pad

--- src/pages/PAD/index.jsx
+++ src/pages/PAD/index.jsx
@@ -35,7 +35,7 @@ const PAD = () => {
                 <input
                   name="start"
                   type="date"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-2xl focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className="bg-white border border-gray-300 text-gray-900 sm:text-sm rounded-2xl block w-full pl-10 p-2.5 focus:ring-orange-900 focus:border-orange-900"
                   placeholder="Select date start"
                 />
               </div>
@@ -61,7 +61,7 @@ const PAD = () => {
                 <input
                   name="end"
                   type="date"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-2xl focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className="bg-white border border-gray-300 text-gray-900 sm:text-sm rounded-2xl block w-full pl-10 p-2.5 focus:ring-orange-900 focus:border-orange-900"
                   placeholder="Select date end"
                 />
               </div>

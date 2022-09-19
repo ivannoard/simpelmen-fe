7ac821849e653fff371d59f4e0d6b08ce9378e84
ivan <ivannovaa.24@gmail.com>
2022-09-19 15:07:28 +0700

    slicing pesanan && detail pesanan

---
+++ src/pages/User/DetailPesanan.jsx
@@ -0,0 +1,144 @@
+import React from "react";
+import { HiOutlineArrowSmLeft } from "react-icons/hi";
+import { useNavigate, useParams } from "react-router-dom";
+import svg from "../../assets/svg";
+
+const DetailPesanan = () => {
+  const { pesananId } = useParams();
+
+  const navigate = useNavigate();
+  function printInvoice(e) {
+    e.preventDefault();
+  }
+  return (
+    <>
+      <section className="overflow-x-auto w-[1440px] lg:w-full">
+        <div className="mb-5">
+          <div className="flex items-center mb-3">
+            <HiOutlineArrowSmLeft className="text-2xl mr-3" />
+            <span
+              className="leading-10 cursor-pointer"
+              onClick={() => navigate(-1)}
+            >
+              Kembali
+            </span>
+          </div>
+          {/* content detail pesanan */}
+          <div className="flex mt-4">
+            <div className="w-1/3 bg-red-500">
+              <h6>Detail Pesanan</h6>
+              <img src={svg.karton} alt="ini-gambar" className="my-5" />
+              <div className="product-title mb-5">
+                <p>Dus Offset</p>
+                <p className="font-semibold">Bentuk Langsungan</p>
+              </div>
+              <div className="product-order-number mb-5">
+                <p>No. Pesanan</p>
+                <p className="font-semibold">001/BIKDK/O/VII/2022</p>
+              </div>
+              <div className="product-order mb-5">
+                <p>Tanggal Pesan</p>
+                <p className="font-semibold">1 Januari 2022</p>
+              </div>
+              <button
+                onClick={(e) => printInvoice(e)}
+                className="button-fill !w-[178px] !h-[40px] !rounded-2xl !px-[20px] !py-[8px] !text-sm "
+              >
+                Cetak Formulir PO
+              </button>
+            </div>
+            <div className="w-2/3 bg-blue-500">
+              <h6>Spesifikasi Produk</h6>
+              {/* <table className="w-full mt-5 mb-3">
+                <tr>
+                  <td className="py-[10px]">Ukuran</td>
+                  <td className="py-[10px]">10x12x8cm</td>
+                </tr>
+                <tr>
+                  <td className="py-[10px]">Bahan</td>
+                  <td className="py-[10px]">Duplex 350 gram</td>
+                </tr>
+                <tr>
+                  <td className="py-[10px]">Desain</td>
+                  <td className="py-[10px]">Swadesain</td>
+                </tr>
+                <tr>
+                  <td className="py-[10px]">Laminasi</td>
+                  <td className="py-[10px]">Laminasi Doff</td>
+                </tr>
+                <tr>
+                  <td className="py-[10px]">Jumlah Pesanan</td>
+                  <td className="py-[10px]">500pcs</td>
+                </tr>
+                <tr>
+                  <td className="py-[10px]">Retribusi</td>
+                  <td className="py-[10px]">Rp.125.000</td>
+                </tr>
+              </table> */}
+              <div className="specification my-5">
+                <div className="flex">
+                  <div className="w-1/2">Nama</div>
+                  <div className="w-1/2">Roikhatul Miskiyah</div>
+                </div>
+                <div className="flex my-[10px]">
+                  <div className="w-1/2">Bahan</div>
+                  <div className="w-1/2">Duplex 350 gram</div>
+                </div>
+                <div className="flex my-[10px]">
+                  <div className="w-1/2">Desain</div>
+                  <div className="w-1/2">Swadesain</div>
+                </div>
+                <div className="flex my-[10px]">
+                  <div className="w-1/2">Laminasi</div>
+                  <div className="w-1/2">Laminasi Doff</div>
+                </div>
+                <div className="flex my-[10px]">
+                  <div className="w-1/2">Jumlah Pesanan</div>
+                  <div className="w-1/2">500pcs</div>
+                </div>
+                <div className="flex my-[10px]">
+                  <div className="w-1/2">Retribusi</div>
+                  <div className="w-1/2">Rp.125.000</div>
+                </div>
+              </div>
+              <h6>Pengiriman</h6>
+              <div className="shipping mb-5">
+                <div className="flex mb-5">
+                  <div className="w-1/2">Nama</div>
+                  <div className="w-1/2">Roikhatul Miskiyah</div>
+                </div>
+                <div className="flex mb-5">
+                  <div className="w-1/2">Nama IKM</div>
+                  <div className="w-1/2">Ikha Catering</div>
+                </div>
+                <div className="flex mb-5">
+                  <div className="w-1/2">No. Handphone</div>
+                  <div className="w-1/2">081-1234-5678</div>
+                </div>
+                <div className="flex mb-5">
+                  <div className="w-1/2">Alamat</div>
+                  <div className="w-1/2">
+                    Jl. Bukit Raya No. 1, Banyumanik, Kota Semarang, Jawa Tengah
+                    123456
+                  </div>
+                </div>
+                <div className="flex mb-5">
+                  <div className="w-1/2">Pengiriman</div>
+                  <div className="w-1/2">Dikirim</div>
+                </div>
+              </div>
+              <h6>Desain</h6>
+              <div className="buttons">
+                <button className="button-fill">Konfirmasi Desain</button>
+                <button className="button-white">Hapus Desain</button>
+                <button className="button-white">Ganti Desain</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default DetailPesanan;
--- src/pages/User/Pesanan.jsx
+++ src/pages/User/Pesanan.jsx
@@ -1,7 +1,75 @@
 import React from "react";
-
+import { HiChevronRight, HiChevronLeft } from "react-icons/hi";
+import { useNavigate } from "react-router-dom";
 const Pesanan = () => {
-  return <div>Pesanan</div>;
+  const navigate = useNavigate();
+  return (
+    <>
+      <section>
+        <div className="flex gap-2 items-center">
+          <label htmlFor="sorting">Menampilkan</label>
+          <select
+            name="sorting"
+            id="sorting"
+            className="w-[50px] rounded-md h-10 p-2 bg-white border border-primary-900"
+          >
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+          </select>
+        </div>
+        <div className="mt-[10px]">
+          <table className="text-center w-[1440px] lg:w-full">
+            <thead>
+              <tr>
+                <th className="bg-[#F29A41] text-white">No. Pesanan</th>
+                <th className="bg-[#F29A41] text-white">Nama Produk</th>
+                <th className="bg-[#F29A41] text-white">Tanggal Pesan</th>
+                <th className="bg-[#F29A41] text-white">Detail Pesanan</th>
+              </tr>
+            </thead>
+            <tbody>
+              {[1, 2, 3, 4, 5].map((item) => (
+                <tr key={item} className="border-b">
+                  <td className="py-5">001/BIKDK/O/VII/2022</td>
+                  <td className="py-5">Bentuk Langsungan</td>
+                  <td className="py-5">1 Januari 2022</td>
+                  <td>
+                    <button
+                      onClick={() => navigate(`/dashboard/detail/${item}`)}
+                      className="border border-secondary-800 rounded-2xl px-[10px] py-2"
+                    >
+                      Sudah Terkonfirmasi
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        <nav
+          className="flex justify-center items-center gap-x-[.375rem] py-2 mt-10"
+          aria-label="pagination"
+        >
+          <button className="button-white-sm !shadow-none hover:!shadow-red !text-xs xs:!text-base !px-3">
+            <HiChevronLeft className="!text-base xs:!text-xl" />
+          </button>
+          <button className="button-gradient-sm !text-xs xs:!text-base">
+            1
+          </button>
+          <button className="button-white-sm !shadow-none hover:!shadow-red !text-xs xs:!text-base">
+            2
+          </button>
+          <button className="button-white-sm !shadow-none hover:!shadow-red !text-xs xs:!text-base">
+            3
+          </button>
+          <button className="button-white-sm !shadow-none hover:!shadow-red !text-xs xs:!text-base !px-3">
+            <HiChevronRight className="!text-base xs:!text-xl" />
+          </button>
+        </nav>
+      </section>
+    </>
+  );
 };
 
 export default Pesanan;

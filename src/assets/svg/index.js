22af828d09579f607ef89a5562d426730fd47f4b
roziqinkhoeru <roziqinkhoeru8@gmail.com>
2022-09-19 14:38:55 +0700

    [layout] add layout empty state at keranjang page

--- src/assets/svg/index.js
+++ src/assets/svg/index.js
@@ -1,26 +1,27 @@
-import loginPage from "./login.svg";
-import registerPage from "./register.svg";
-import logoSimpelmen from "./Logo Simpelmen.svg";
-import facebookIcon from "./facebook.svg";
-import twitterIcon from "./twitter.svg";
-import youtubeIcon from "./youtube.svg";
-import instagramIcon from "./instagram.svg";
-import bgHero from "./bg-hero.svg";
-import heroImage from "./hero-img.svg";
-import productQuality from "./product-quality.svg";
-import savingMoney from "./saving-money.svg";
-import quantity from "./quantity.svg";
-import flowOrder1 from "./flow-order-1.svg";
-import flowOrder2 from "./flow-order-2.svg";
-import flowOrder3 from "./flow-order-3.svg";
-import flowOrder4 from "./flow-order-4.svg";
-import flowOrder5 from "./flow-order-5.svg";
-import flowOrder6 from "./flow-order-6.svg";
-import flowOrder7 from "./flow-order-7.svg";
-import flowOrder8 from "./flow-order-8.svg";
-import bgOurProduct from "./bg-our-product.svg";
-import karton from "./karton.svg";
-import LogoDashboardUser from "./dashboard-user-logo.svg";
+import loginPage from './login.svg';
+import registerPage from './register.svg';
+import logoSimpelmen from './Logo Simpelmen.svg';
+import facebookIcon from './facebook.svg';
+import twitterIcon from './twitter.svg';
+import youtubeIcon from './youtube.svg';
+import instagramIcon from './instagram.svg';
+import bgHero from './bg-hero.svg';
+import heroImage from './hero-img.svg';
+import productQuality from './product-quality.svg';
+import savingMoney from './saving-money.svg';
+import quantity from './quantity.svg';
+import flowOrder1 from './flow-order-1.svg';
+import flowOrder2 from './flow-order-2.svg';
+import flowOrder3 from './flow-order-3.svg';
+import flowOrder4 from './flow-order-4.svg';
+import flowOrder5 from './flow-order-5.svg';
+import flowOrder6 from './flow-order-6.svg';
+import flowOrder7 from './flow-order-7.svg';
+import flowOrder8 from './flow-order-8.svg';
+import bgOurProduct from './bg-our-product.svg';
+import karton from './karton.svg';
+import LogoDashboardUser from './dashboard-user-logo.svg';
+import EmptyKeranjang from './empty-keranjang.svg';
 
 const svg = {
   loginPage,
@@ -46,6 +47,7 @@ const svg = {
   bgOurProduct,
   karton,
   LogoDashboardUser,
+  EmptyKeranjang,
 };
 
 export default svg;
--- src/pages/Keranjang/index.jsx
+++ src/pages/Keranjang/index.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import CardCart from "../../components/Card/CardCart";
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import CardCart from '../../components/Card/CardCart';
 
-import { dummyImg } from "../../assets/image";
-import { HiOutlineArrowSmLeft } from "react-icons/hi";
+import { dummyImg } from '../../assets/image';
+import { HiOutlineArrowSmLeft } from 'react-icons/hi';
+import svg from '../../assets/svg';
 
 const Keranjang = () => {
   const navigate = useNavigate();
@@ -12,73 +13,108 @@ const Keranjang = () => {
     setIsCheck(!isCheck);
   };
   const handleDelete = () => {
-    console.log("delete");
+    console.log('delete');
   };
 
   const dummyData = [
     {
       id: 1,
       produkImg: dummyImg.kotakBerdiri,
-      altImg: "Kotak Berdiri",
-      kategori: "Dus Offset",
-      jenis: "Kotak Berdiri",
+      altImg: 'Kotak Berdiri',
+      kategori: 'Dus Offset',
+      jenis: 'Kotak Berdiri',
     },
     {
       id: 2,
       produkImg: dummyImg.boxTentengan,
-      altImg: "Box Tentengan",
-      kategori: "Karton",
-      jenis: "Box Tentengan",
+      altImg: 'Box Tentengan',
+      kategori: 'Karton',
+      jenis: 'Box Tentengan',
     },
     {
       id: 3,
       produkImg: dummyImg.topBottom,
-      altImg: "Top Bottom",
-      kategori: "Dus Offset",
-      jenis: "Top Bottom",
+      altImg: 'Top Bottom',
+      kategori: 'Dus Offset',
+      jenis: 'Top Bottom',
     },
     {
       id: 4,
       produkImg: dummyImg.bentukSegitiga,
-      altImg: "Bentuk Segitiga",
-      kategori: "Dus Offset",
-      jenis: "Bentuk Segitiga",
+      altImg: 'Bentuk Segitiga',
+      kategori: 'Dus Offset',
+      jenis: 'Bentuk Segitiga',
     },
     {
       id: 5,
       produkImg: dummyImg.boxModelPizza,
-      altImg: "Box Model Pizza",
-      kategori: "Karton",
-      jenis: "Box Model Pizza",
+      altImg: 'Box Model Pizza',
+      kategori: 'Karton',
+      jenis: 'Box Model Pizza',
     },
   ];
+  const dummy = false;
   return (
     <>
-      <main className='containers'>
-        <div className='mb-5 flex'>
-          <Link to='/' className='flex items-center mb-3'>
-            <HiOutlineArrowSmLeft className='text-2xl mr-3' />
-            <span className='leading-10'>Kembali</span>
+      <main className="containers">
+        <div className="mb-5 flex">
+          <Link
+            to="/"
+            className="flex items-center mb-3"
+          >
+            <HiOutlineArrowSmLeft className="text-2xl mr-3" />
+            <span className="leading-10">Kembali</span>
           </Link>
         </div>
-        <section id='cart' className='mb-10'>
-          {dummyData.map((item, index) => {
-            return (
-              <CardCart
-                key={index}
-                {...item}
-                handleCheck={handleCheck}
-                isCheck={isCheck}
-                handleDelete={handleDelete}
-              />
-            );
-          })}
-        </section>
-        <div className='flex justify-center mb-9'>
-          <button className='button-fill' onClick={() => navigate("/")}>
-            Lanjutkan Pesanan
-          </button>
-        </div>
+        {/* dummy condition */}
+        {dummy ? (
+          <>
+            <section
+              id="cart"
+              className="mb-10"
+            >
+              {dummyData.map((item, index) => {
+                return (
+                  <CardCart
+                    key={index}
+                    {...item}
+                    handleCheck={handleCheck}
+                    isCheck={isCheck}
+                    handleDelete={handleDelete}
+                  />
+                );
+              })}
+            </section>
+            <div className="flex justify-center mb-9">
+              <button
+                className="button-fill"
+                onClick={() => navigate('/')}
+              >
+                Lanjutkan Pesanan
+              </button>
+            </div>
+          </>
+        ) : (
+          <section className="pt-9 pb-12 2xsm:pb-28 xmd:pb-40">
+            <div className="w-4/5 md:w-[33.75rem] mx-auto">
+              <div className="w-full px-9">
+                <img
+                  src={svg.EmptyKeranjang}
+                  alt="empty-keranjang"
+                  className="w-full mb-10"
+                />
+              </div>
+              <h3 className="text-center mb-10">
+                Keranjang Anda masih kosong!
+              </h3>
+              <div className="flex justify-center">
+                <button className="button-gradient-sm 2xsm:button-gradient">
+                  Pesan Sekarang
+                </button>
+              </div>
+            </div>
+          </section>
+        )}
       </main>
     </>
   );

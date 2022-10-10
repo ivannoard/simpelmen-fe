e13090f07c921b1473bd9f3ceb74bdcb48f7a2dc
ivan <ivannovaa.24@gmail.com>
2022-10-10 11:20:21 +0700

    add user experience when login admin

--- src/pages/Admin/index.jsx
+++ src/pages/Admin/index.jsx
@@ -6,31 +6,34 @@ const Admin = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (!admin) navigate("/admin/login");
-    switch (parseInt(admin)) {
-      case 1:
-        return navigate("/admin/cs/dashboard/");
-      case 2:
-        return navigate("/admin/desain/dashboard/");
-      case 3:
-        return navigate("/admin/gudang/dashboard/");
-      case 4:
-        return navigate("/admin/kasir/dashboard/");
-      case 5:
-        return navigate("/admin/produksi/dashboard/");
-      case 6:
-        return navigate("/admin/super/dashboard/");
-      case 7:
-        return navigate("/admin/tu/dashboard/");
-      default:
-        break;
-    }
+    setTimeout(() => {
+      if (!admin) navigate("/admin/login");
+      switch (parseInt(admin)) {
+        case 1:
+          return navigate("/admin/cs/dashboard/");
+        case 2:
+          return navigate("/admin/desain/dashboard/");
+        case 3:
+          return navigate("/admin/gudang/dashboard/");
+        case 4:
+          return navigate("/admin/kasir/dashboard/");
+        case 5:
+          return navigate("/admin/produksi/dashboard/");
+        case 6:
+          return navigate("/admin/super/dashboard/");
+        case 7:
+          return navigate("/admin/tu/dashboard/");
+        default:
+          break;
+      }
+    }, 2000);
   }, [admin, navigate]);
 
   return (
     <>
-      <h1>redirect</h1>
-      {}
+      <div className="flex justify-center items-center h-screen w-screen">
+        <h1 className="text-secondary-800 animate-bounce">Authorizing</h1>
+      </div>
     </>
   );
 };

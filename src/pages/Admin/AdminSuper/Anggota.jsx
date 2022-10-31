59588441e5a765c7a18aaa11d1e9c6d98b4b6cef
roziqinkhoeru <roziqinkhoeru8@gmail.com>
2022-10-31 11:11:19 +0700

    [update] dashboard super admin page anggota

--- src/pages/Admin/AdminSuper/Anggota.jsx
+++ src/pages/Admin/AdminSuper/Anggota.jsx
@@ -1,33 +1,61 @@
-import React, { useState } from "react";
-import { BsSearch } from "react-icons/bs";
-import { FaTrash } from "react-icons/fa";
-import { HiChevronLeft, HiChevronRight } from "react-icons/hi";
-import ModalAddAdmin from "./components/ModalAddAdmin";
+import React, { useState } from 'react';
+import { BsSearch, BsPlus } from 'react-icons/bs';
+import { FaTrash } from 'react-icons/fa';
+import { HiChevronLeft, HiChevronRight } from 'react-icons/hi';
+import ModalsAddAdmin from './components/ModalsAddAdmin';
+import ModalsEditAdmin from './components/ModalsEditAdmin';
 
 const Anggota = () => {
-  const [toggle, setToggle] = useState(false);
+  const [isOpenModal, setIsOpenModal] = useState(false);
+  const [isOpenModalEdit, setIsOpenModalEdit] = useState(false);
+  const [idAdmin, setIdAdmin] = useState();
+
+  const closeModal = () => {
+    setIsOpenModal(false);
+  };
+
+  const closeModalEdit = () => {
+    setIsOpenModalEdit(false);
+  };
+
+  const detailModalHandling = () => {
+    setIsOpenModal(true);
+  };
+
+  const detailModalHandlingEdit = (id) => {
+    setIsOpenModalEdit(true);
+    setIdAdmin(id);
+  };
+
+  const submitAdminHandler = (e) => {};
+  const submitEditAdminHandler = (e) => {};
+
   return (
     <>
       <section>
-        {toggle && <ModalAddAdmin setToggle={setToggle} />}
-        <div className=" border-b border-orange-900">
-          <h3 className="font-semibold">Admin</h3>
+        <div className="border-b border-orange-900 mb-6">
+          <h3 className="font-semibold pb-3">Admin</h3>
         </div>
-        <h6 className="mt-6">Tabel Admin</h6>
-        <button
-          onClick={() => setToggle(true)}
-          className="bg-primary-900 text-white font-semibold my-4 py-4 px-3 rounded-2xl w-[280px]"
-        >
-          Tambah Admin
-        </button>
-        {/*  */}
-        <div className="flex items-center justify-between">
-          <div className="flex gap-2 items-center">
+        <div className="flex flex-col gap-y-4 xs:gap-y-0 xs:flex-row xs:items-center justify-between mb-4">
+          <h6 className="">Tabel Admin</h6>
+          <div>
+            <button
+              onClick={detailModalHandling}
+              className="button-fill !pl-4 flex items-center"
+            >
+              <BsPlus className="text-2xl mr-2 fill-white" />
+              Tambah Admin
+            </button>
+          </div>
+        </div>
+
+        <div className="flex items-center justify-between mb-4">
+          <div className="flex gap-2 items-center mr-4">
             <label htmlFor="sorting">Menampilkan</label>
             <select
               name="sorting"
               id="sorting"
-              className="w-[50px] rounded-md h-10 p-2 bg-white border border-primary-900"
+              className="w-[50px] rounded h-10 p-2 bg-white border border-primary-900"
             >
               <option value="1">1</option>
               <option value="2">2</option>
@@ -36,70 +64,107 @@ const Anggota = () => {
           </div>
           <div className="flex relative top-2 flex-col mb-4">
             <input
-              type="email"
-              className="input-field !rounded-full !py-2"
+              type="search"
+              className="input-field !rounded-full !py-2 !pl-14"
               placeholder="Cari"
-              name="email"
+              name="search"
               required
               autoComplete="on"
               // onChange={handleChange}
             />
-            <BsSearch className="absolute text-2xl top-[10px] left-5 fill-secondary-800" />
+            <BsSearch className="absolute text-xl top-3 left-6 fill-secondary-800" />
           </div>
         </div>
-        {/*  */}
-        <table className="table-auto mt-4 w-[1440px] lg:w-full">
-          <thead>
-            <tr className="bg-orange-900">
-              <td className="text-white text-center py-3">No</td>
-              <td className="text-white text-center py-3">Nama</td>
-              <td className="text-white text-center py-3">Email</td>
-              <td className="text-white text-center py-3">Posisi</td>
-              <td className="text-white text-center py-3">Aksi</td>
-            </tr>
-          </thead>
-          <tbody>
-            {[1, 2, 3, 4, 5].map((item) => (
-              <tr className="border-b">
-                <td className="text-center py-3">1</td>
-                <td className="text-center py-3">Ivan</td>
-                <td className="text-center py-3">ivan@gmail.com</td>
-                <td className="text-center py-3">Desain</td>
-                <td className="text-center py-3">
-                  <div className="flex items-center justify-center gap-2">
-                    <button className="border bg-white py-2 px-7 rounded-lg">
-                      Edit
-                    </button>
-                    <div className="bg-primary-900 p-3 rounded-lg">
-                      <FaTrash fill="#FFFFFF" size={16} />
-                    </div>
-                  </div>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-        <nav
-          className="flex justify-end items-center gap-x-[.375rem] py-2 mt-5"
-          aria-label="pagination"
-        >
-          <button className="button-white-sm !shadow-none hover:!shadow-red !text-xs xs:!text-base !px-3">
-            <HiChevronLeft className="!text-base xs:!text-xl" />
-          </button>
-          <button className="button-gradient-sm !text-xs xs:!text-base">
-            1
-          </button>
-          <button className="button-white-sm !shadow-none hover:!shadow-red !text-xs xs:!text-base">
-            2
-          </button>
-          <button className="button-white-sm !shadow-none hover:!shadow-red !text-xs xs:!text-base">
-            3
-          </button>
-          <button className="button-white-sm !shadow-none hover:!shadow-red !text-xs xs:!text-base !px-3">
-            <HiChevronRight className="!text-base xs:!text-xl" />
-          </button>
-        </nav>
+
+        <article id="tableAdmin">
+          <div className="overflow-x-auto">
+            <table className="table-auto mb-4 w-full">
+              <thead>
+                <tr className="bg-orange-900">
+                  <th className="text-white text-center p-3 min-w-[54px]">
+                    No
+                  </th>
+                  <th className="text-white text-center p-3 min-w-[180px]">
+                    Nama
+                  </th>
+                  <th className="text-white text-center p-3 min-w-[200px]">
+                    Email
+                  </th>
+                  <th className="text-white text-center p-3 min-w-[90px]">
+                    Posisi
+                  </th>
+                  <th className="text-white text-center p-3 min-w-[180px]">
+                    Aksi
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {[1, 2, 3, 4, 5].map((item, index) => (
+                  <tr
+                    className="border-b"
+                    key={index}
+                  >
+                    <td className="text-center p-3">{index + 1}</td>
+                    <td className="text-center p-3">Ivan Nova Rivaldo</td>
+                    <td className="text-center p-3">
+                      rivaldonovaivan@gmail.com
+                    </td>
+                    <td className="text-center p-3">Desain</td>
+                    <td className="text-center p-3">
+                      <div className="flex items-center justify-center gap-2">
+                        <button
+                          className="bg-white border py-3 px-4 rounded-lg text-sm transition-200 hover:border-orange-900"
+                          onClick={() => detailModalHandlingEdit(item)}
+                        >
+                          Edit
+                        </button>
+                        <div className="button-fill !p-[15px]">
+                          <FaTrash className="fill-white text-base" />
+                        </div>
+                      </div>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+          <nav
+            className="flex justify-end items-center gap-x-[.375rem] py-2 mt-2"
+            aria-label="pagination"
+          >
+            <button className="button-white-sm !shadow-none hover:!shadow-red !text-xs xs:!text-base !px-3">
+              <HiChevronLeft className="!text-base xs:!text-xl" />
+            </button>
+            <button className="button-gradient-sm !text-xs xs:!text-base">
+              1
+            </button>
+            <button className="button-white-sm !shadow-none hover:!shadow-red !text-xs xs:!text-base">
+              2
+            </button>
+            <button className="button-white-sm !shadow-none hover:!shadow-red !text-xs xs:!text-base">
+              3
+            </button>
+            <button className="button-white-sm !shadow-none hover:!shadow-red !text-xs xs:!text-base !px-3">
+              <HiChevronRight className="!text-base xs:!text-xl" />
+            </button>
+          </nav>
+        </article>
       </section>
+
+      {/* Modal Add Admin */}
+      <ModalsAddAdmin
+        isOpen={isOpenModal}
+        closeModal={closeModal}
+        submitHandler={submitAdminHandler}
+      />
+
+      {/* Modal Edit Admin */}
+      <ModalsEditAdmin
+        isOpen={isOpenModalEdit}
+        closeModal={closeModalEdit}
+        submitHandler={submitEditAdminHandler}
+        idAdmin={idAdmin}
+      />
     </>
   );
 };

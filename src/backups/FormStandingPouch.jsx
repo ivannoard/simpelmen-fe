fe94d533c123d54bf2173f860a32a59ba34bba9c
roziqinkhoeru <roziqinkhoeru8@gmail.com>
2022-12-02 14:04:36 +0700

    [backups] after conflict merge

---
+++ src/backups/FormStandingPouch.jsx
@@ -0,0 +1,422 @@
+// import React, { useState } from 'react';
+// import { Buffer } from 'buffer';
+// import { BsCartPlus, BsFillCloudPlusFill } from 'react-icons/bs';
+// import { getSpecification, postOrder } from '../../../services/api';
+// import { IoIosArrowDown } from 'react-icons/io';
+// import dummyData from './dummyDataForm';
+// import regex from '../../../services/regex';
+// import ErrorMessage from '../../../components/Alerts/ErrorMessage';
+// import { MdDelete, MdPermMedia } from 'react-icons/md';
+// import { CgSpinner } from 'react-icons/cg';
+// import { useEffect } from 'react';
+
+// const { number: NUMBER_REGEX } = regex;
+
+// const FormStandingPouch = ({ productId, setAlertSuccess, setAlertFail }) => {
+//   const [fields, setFields] = useState({});
+//   const [specification, setSpecification] = useState(null);
+//   const user = localStorage.getItem('user');
+//   const [isUploadDesign, setIsUploadDesign] = useState(false);
+//   const [isLoading, setIsLoading] = useState(false);
+//   const [isLetterBtnLoading, setIsLetterBtnLoading] = useState(false);
+//   const [isIconBtnLoading, setIsIconBtnLoading] = useState(false);
+//   const [validFields, setValidFields] = useState({
+//     order_specification: false,
+//     order_design: false,
+//     order_quantity: false,
+//     order_design_image: false,
+//   });
+
+//   function handleChange(e) {
+//     e.preventDefault();
+//     setFields({
+//       ...fields,
+//       [e.target.getAttribute('name')]: e.target.value,
+//     });
+//   }
+
+//   function handleDesign(e) {
+//     e.preventDefault();
+//     if (e.target.value === 'swadesign') {
+//       setIsUploadDesign(true);
+//     } else {
+//       setIsUploadDesign(false);
+//     }
+//   }
+
+//   function handleFile(e) {
+//     if (e.target.files[0] !== '' && e.target.files[0].size < 3000000) {
+//       const file = e.target.files[0];
+//       if (file) {
+//         const reader = new FileReader();
+//         reader.onload = (e) => {
+//           setFields({
+//             ...fields,
+//             order_design_image: {
+//               name: file.name,
+//               data: Buffer.from(e.target.result).toString('base64'),
+//             },
+//           });
+//         };
+//         reader.readAsDataURL(file);
+//       }
+//     }
+//   }
+
+//   function removeFile() {
+//     setFields({
+//       ...fields,
+//       order_design_image: null,
+//     });
+//   }
+
+//   const validateFieldsHandler = (e) => {
+//     const { name, value } = e.target;
+//     switch (name) {
+//       case 'order_specification':
+//         setValidFields({
+//           ...validFields,
+//           order_specification: value !== '' && value !== '0',
+//         });
+//         break;
+//       case 'order_design':
+//         setValidFields({
+//           ...validFields,
+//           order_design: value !== '' && value !== '0',
+//         });
+//         break;
+//       case 'order_quantity':
+//         setValidFields({
+//           ...validFields,
+//           order_quantity: NUMBER_REGEX.test(value),
+//         });
+//         break;
+//       default:
+//         break;
+//     }
+//   };
+
+//   const valids =
+//     fields.order_design === 'swadesign'
+//       ? validFields.order_specification &&
+//         validFields.order_design &&
+//         validFields.order_quantity &&
+//         validFields.order_design_image
+//       : validFields.order_specification &&
+//         validFields.order_design &&
+//         validFields.order_quantity;
+
+//   useEffect(() => {
+//     const getSpec = () => {
+//       getSpecification
+//         .get(`${productId}`)
+//         .then((res) => {
+//           setSpecification(res.data);
+//         })
+//         .catch((err) => {
+//           console.log(err);
+//         });
+//     };
+
+//     getSpec();
+//   }, [productId]);
+
+//   async function handleSubmit(e, isLetterBtn, isIconBtn) {
+//     e.preventDefault();
+
+//     const finalSpesification =
+//       fields.order_design === 'swadesign'
+//         ? {
+//             panjang_1: parseInt(fields?.order_specification.split(' ')[0]),
+//             lebar_1: parseInt(fields?.order_specification.split(' ')[3]),
+//             order_design: fields?.order_design,
+//             order_quantity: parseInt(fields?.order_quantity),
+//             order_design_image: fields?.order_design_image.data,
+//             order_finishing_id: fields?.order_specification.split(' ')[5],
+//           }
+//         : {
+//             panjang_1: parseInt(fields?.order_specification.split(' ')[0]),
+//             lebar_1: parseInt(fields?.order_specification.split(' ')[3]),
+//             order_design: fields?.order_design,
+//             order_quantity: parseInt(fields?.order_quantity),
+//             order_finishing_id: fields?.order_specification.split(' ')[5],
+//           };
+//     setIsLoading(true);
+//     if (isLetterBtn) {
+//       setIsLetterBtnLoading(true);
+//     } else if (isIconBtn) {
+//       setIsIconBtnLoading(true);
+//     }
+
+//     console.log(finalSpesification);
+
+//     if (user) {
+//       if (valids) {
+//         await postOrder
+//           .post(`/cart/${productId}`, finalSpesification, {
+//             headers: {
+//               'x-access-token': `${JSON.parse(user).data.token}`,
+//             },
+//           })
+//           .then(() => {
+//             setAlertSuccess(true);
+//             setIsLoading(false);
+//             setIsLetterBtnLoading(false);
+//             setIsIconBtnLoading(false);
+//           })
+//           .catch((err) => {
+//             console.log(err);
+//             removeFile();
+//             setAlertFail(true);
+//             setIsLoading(false);
+//             setIsLetterBtnLoading(false);
+//             setIsIconBtnLoading(false);
+//           });
+//       } else {
+//         setAlertFail(true);
+//         setIsLoading(false);
+//         setIsLetterBtnLoading(false);
+//         setIsIconBtnLoading(false);
+//       }
+//     } else {
+//       setAlertFail(true);
+//       setIsLoading(false);
+//       setIsLetterBtnLoading(false);
+//       setIsIconBtnLoading(false);
+//     }
+//   }
+
+//   return (
+//     <form>
+//       {/* spesifikasi */}
+//       <div className="relative">
+//         <label
+//           htmlFor="spesifikasi"
+//           className="block mb-2 text-sm font-medium text-gray-700"
+//         >
+//           Spesifikasi
+//           <span className="text-primary-900 font-semibold">*</span>
+//         </label>
+//         <select
+//           id="spesifikasi"
+//           name="order_specification"
+//           required
+//           onChange={(e) => {
+//             handleChange(e);
+//             validateFieldsHandler(e);
+//           }}
+//           className={`input-field-select-xs ${
+//             fields.order_specification &&
+//             !validFields.order_specification &&
+//             'field-error'
+//           }`}
+//           aria-invalid={validFields.order_specification ? 'false' : 'true'}
+//           aria-describedby="orderSpecificationField"
+//         >
+//           <option value="0">Pilih Spesifikasi</option>
+//           {specification?.map((item, index) => (
+//             <option
+//               value={`${item.product_sizes.product_size_length} cm X ${item.product_sizes.product_size_width} cm ${item.product_finishings.product_finishing_id}`}
+//               key={index}
+//             >
+//               {item.product_sizes.product_size_length} cm X{' '}
+//               {item.product_sizes.product_size_width} cm{' '}
+//               {item.product_finishings.product_finishing_name}
+//             </option>
+//           ))}
+//         </select>
+//         <IoIosArrowDown className="absolute right-4 top-[43px] text-lg fill-gray-400" />
+//         {fields.order_specification && !validFields.order_specification && (
+//           <ErrorMessage
+//             referenceId="orderSpecifiationField"
+//             message="Masukkan spesifikasi produk dengan benar."
+//             isPasswordField={false}
+//           />
+//         )}
+//       </div>
+//       {/* order design */}
+//       <div className="mt-4 relative">
+//         <label
+//           htmlFor="desain"
+//           className="block mb-2 text-sm font-medium text-gray-700"
+//         >
+//           Desain
+//           <span className="text-primary-900 font-semibold">*</span>
+//         </label>
+//         <select
+//           id="desain"
+//           name="order_design"
+//           required
+//           onChange={(e) => {
+//             handleChange(e);
+//             handleDesign(e);
+//             validateFieldsHandler(e);
+//           }}
+//           className={`input-field-select-xs ${
+//             fields.order_design && !validFields.order_design && 'field-error'
+//           }`}
+//           aria-invalid={validFields.order_design ? 'false' : 'true'}
+//           aria-describedby="orderDesignField"
+//         >
+//           <option value="0">Pilih Desain</option>
+//           {dummyData.dummyDesign.map((item, index) => (
+//             <option
+//               value={item.value}
+//               key={index}
+//             >
+//               {item.desc}
+//             </option>
+//           ))}
+//         </select>
+//         <IoIosArrowDown className="absolute right-4 top-[43px] text-lg fill-gray-400" />
+//         {fields.order_design && !validFields.order_design && (
+//           <ErrorMessage
+//             referenceId="orderDesignField"
+//             message="Pilih desain yang diinginkan."
+//             isPasswordField={false}
+//           />
+//         )}
+//       </div>
+//       {isUploadDesign && (
+//         <div className="mt-4">
+//           <label
+//             htmlFor="uploadDesign"
+//             className="block mb-2 text-sm font-medium text-gray-700"
+//           >
+//             Unggah Desain
+//             <span className="text-primary-900 font-semibold">*</span>
+//           </label>
+//           <div className="w-full p-12 border-2 border-gray-500 border-dashed rounded-lg flex items-center justify-center bg-gray-100">
+//             <div>
+//               <div className="relative mb-5">
+//                 <input
+//                   type="file"
+//                   id="uploadDesign"
+//                   name="order_design_image"
+//                   className="absolute appearance-none w-full h-full opacity-0 cursor-pointer"
+//                   required
+//                   onChange={(e) => {
+//                     setValidFields({
+//                       ...validFields,
+//                       order_design_image:
+//                         e.target.value !== '' &&
+//                         e.target.files[0].size < 3000000,
+//                     });
+//                     handleFile(e);
+//                   }}
+//                   aria-invalid={
+//                     validFields.order_design_image ? 'false' : 'true'
+//                   }
+//                   aria-describedby="orderDesignImgField"
+//                 />
+//                 <div className="button-fill cursor-pointer">
+//                   <BsFillCloudPlusFill className="inline-block text-xl fill-white mr-2" />
+//                   Upload
+//                 </div>
+//               </div>
+//               <p className="text-center font-semibold">Support files</p>
+//               <p className="text-center text-sm">PDF, JPG, PNG</p>
+//             </div>
+//           </div>
+//           {!validFields.order_design_image && (
+//             <ErrorMessage
+//               referenceId="orderDesignImgField"
+//               message="File harus kurang dari 3MB"
+//               isPasswordField={false}
+//             />
+//           )}
+//           {fields.order_design_image && validFields.order_design_image && (
+//             <div className="flex items-center justify-between rounded-lg bg-orange-600 px-5 py-4 mt-4">
+//               <p className="truncate">
+//                 <MdPermMedia className="text-2xl fill-dark mr-3 inline-block" />
+//                 {fields.order_design_image.name}
+//               </p>
+
+//               <button
+//                 onClick={removeFile}
+//                 type="button"
+//               >
+//                 <MdDelete className="text-2xl fill-red-600 ml-4" />
+//               </button>
+//             </div>
+//           )}
+//         </div>
+//       )}
+//       {/* order quantity */}
+//       <div className="mt-4">
+//         <label
+//           htmlFor="jumlah"
+//           className="block mb-2 text-sm font-medium text-gray-700"
+//         >
+//           Jumlah Pesanan
+//           <span className="text-primary-900 font-semibold">*</span>
+//         </label>
+//         <div className="relative">
+//           <input
+//             type="text"
+//             id="jumlah"
+//             name="order_quantity"
+//             placeholder="Masukkan Jumlah Pesanan"
+//             required
+//             onChange={(e) => {
+//               handleChange(e);
+//               validateFieldsHandler(e);
+//             }}
+//             className={`input-field-xs !pr-12 ${
+//               fields.order_quantity &&
+//               !validFields.order_quantity &&
+//               'field-error'
+//             }`}
+//             aria-invalid={validFields.order_quantity ? 'false' : 'true'}
+//             aria-describedby="orderQuantityField"
+//           />
+//           <span className="text-gray-400 absolute right-3 top-[11px]">pcs</span>
+//           {fields.order_quantity && !validFields.order_quantity && (
+//             <ErrorMessage
+//               referenceId="orderQuantityField"
+//               message="Masukkan jumlah pesanan dengan benar dan sesuai."
+//               isPasswordField={false}
+//             />
+//           )}
+//         </div>
+//       </div>
+//       {/* button */}
+//       <div className="buttons flex justify-end mt-8 gap-5">
+//         <button
+//           onClick={(e) => handleSubmit(e, true, false)}
+//           className={`button-fill !py-4 flex items-center justify-center ${
+//             isLoading ? '!bg-primary-600' : ''
+//           }`}
+//           type="submit"
+//           disabled={isLoading}
+//         >
+//           {isLetterBtnLoading ? (
+//             <>
+//               <CgSpinner className="animate-spin text-xl mr-2 icon-white" />
+//               Pesan Sekarang
+//             </>
+//           ) : (
+//             <>Pesan Sekarang</>
+//           )}
+//         </button>
+//         <button
+//           className={`button-white !p-4 ${isLoading ? '!bg-gray-100' : ''}`}
+//           onClick={(e) => handleSubmit(e, false, true)}
+//           type="submit"
+//           disabled={isLoading}
+//         >
+//           {isIconBtnLoading ? (
+//             <CgSpinner className="animate-spin text-xl" />
+//           ) : (
+//             <BsCartPlus
+//               size={20}
+//               className="mx-auto"
+//             />
+//           )}
+//         </button>
+//       </div>
+//     </form>
+//   );
+// };
+
+// export default FormStandingPouch;
---
+++ src/backups/kemasan-index.jsx
@@ -0,0 +1,122 @@
+// import React, { useEffect, useState } from 'react';
+// import { Link } from 'react-router-dom';
+// import CardProduct from '../../components/Card/CardProduct';
+// import { HiOutlineArrowSmLeft } from 'react-icons/hi';
+// import { HiChevronRight, HiChevronLeft } from 'react-icons/hi';
+// import useProducts from '../../hooks/useProductDetail';
+// import CardSkeleton from '../../components/Skeletons/CardSkeleton';
+
+// const Kemasan = () => {
+//   const [active, setActive] = useState('Semua Kemasan');
+//   const [productData, setProductData] = useState();
+//   const type = [
+//     'Semua Kemasan',
+//     'Karton',
+//     'Dus Offset',
+//     'Sablon Plastik, Pouch, Dus',
+//     'Sticker',
+//     'Standing Pouch',
+//   ];
+//   const { data, isLoading } = useProducts(
+//     'https://simpelmen.herokuapp.com/api/product'
+//   );
+
+//   function handleActive(type) {
+//     setActive(type);
+//     if (type === 'Semua Kemasan') {
+//       setProductData(data);
+//     } else {
+//       const filteredData = data.filter(
+//         (item) => item.jenis_products.jenis_product_name === type
+//       );
+//       setProductData(filteredData);
+//     }
+//   }
+//   useEffect(() => {
+//     if (!isLoading) setProductData(data);
+//   }, [data, isLoading]);
+
+//   return (
+//     <>
+//       <main className="containers">
+//         <section
+//           id="categoryKemasan"
+//           className="mt-0 xs:mt-7 mb-12 2xsm:mb-60/sp"
+//         >
+//           <div className="mb-5 flex">
+//             <Link
+//               to="/"
+//               className="flex items-center mb-3"
+//             >
+//               <HiOutlineArrowSmLeft className="text-2xl mr-3" />
+//               <span className="leading-10">Kembali</span>
+//             </Link>
+//           </div>
+//           <div className="flex items-center gap-2 xs:gap-3 md:gap-4 flex-wrap">
+//             {type.map((item, index) => (
+//               <button
+//                 key={index}
+//                 onClick={() => handleActive(item)}
+//                 className={
+//                   active === item
+//                     ? 'button-gradient-sm !text-xs xs:!text-base !rounded-full'
+//                     : 'button-white-sm !text-xs xs:!text-base !rounded-full'
+//                 }
+//               >
+//                 {item}
+//               </button>
+//             ))}
+//           </div>
+//         </section>
+//         <section
+//           id="kemasan"
+//           className="mb-9"
+//         >
+//           <div className="w-full grid grid-cols-8 md:grid-cols-12 gap-3 2xsm:gap-5 xl:gap-7 mb-60/sp">
+//             {isLoading
+//               ? [1, 2, 3, 4, 5, 6, 7, 8].map((item) => (
+//                   <div
+//                     className="col-span-4 lg:col-span-3"
+//                     key={item}
+//                   >
+//                     <CardSkeleton />
+//                   </div>
+//                 ))
+//               : productData?.map((item, index) => {
+//                   return (
+//                     <div
+//                       className="col-span-4 lg:col-span-3"
+//                       key={index}
+//                     >
+//                       <CardProduct {...item} />
+//                     </div>
+//                   );
+//                 })}
+//           </div>
+//           <nav
+//             className="flex justify-center items-center gap-x-[.375rem] py-2"
+//             aria-label="pagination"
+//           >
+//             <button className="button-white-sm !shadow-none hover:!shadow-red !text-xs xs:!text-base !px-3">
+//               <HiChevronLeft className="!text-base xs:!text-xl" />
+//             </button>
+//             <button className="button-gradient-sm !text-xs xs:!text-base">
+//               1
+//             </button>
+//             <button className="button-white-sm !shadow-none hover:!shadow-red !text-xs xs:!text-base">
+//               2
+//             </button>
+//             <button className="button-white-sm !shadow-none hover:!shadow-red !text-xs xs:!text-base">
+//               3
+//             </button>
+//             <button className="button-white-sm !shadow-none hover:!shadow-red !text-xs xs:!text-base !px-3">
+//               <HiChevronRight className="!text-base xs:!text-xl" />
+//             </button>
+//           </nav>
+//         </section>
+//       </main>
+//     </>
+//   );
+// };
+
+// export default Kemasan;

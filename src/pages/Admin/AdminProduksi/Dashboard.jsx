13dabd9ede1a2bfd7f8eb37757fa2a30b609ce12
roziqinkhoeru <roziqinkhoeru8@gmail.com>
2022-12-17 15:47:25 +0700

    [update] loading state with skeleton table
    admin produkse

--- src/pages/Admin/AdminProduksi/Dashboard.jsx
+++ src/pages/Admin/AdminProduksi/Dashboard.jsx
@@ -1,20 +1,20 @@
-import React, { useEffect, useState } from "react";
-import { BsSearch } from "react-icons/bs";
-import { IoIosArrowDown } from "react-icons/io";
-import Alerts from "../../../components/Alerts";
-import Pagination from "../../../components/Pagination";
-import { adminProduksi } from "../../../services/api";
-import ModalDetail from "./components/ModalDetail";
+import React, { useEffect, useState } from 'react';
+import { BsSearch } from 'react-icons/bs';
+import { IoIosArrowDown } from 'react-icons/io';
+import Alerts from '../../../components/Alerts';
+import Pagination from '../../../components/Pagination';
+import { adminProduksi } from '../../../services/api';
+import ModalDetail from './components/ModalDetail';
 
 const Dashboard = () => {
-  const user = localStorage.getItem("admin");
+  const user = localStorage.getItem('admin');
   const parseUser = JSON.parse(user);
   const [productData, setProductData] = useState();
   const [isOpenModal, setIsOpenModal] = useState(false);
   const [detailData, setDetailData] = useState();
   const [alerts, setAlerts] = useState(false);
   const [alertFail, setAlertFail] = useState(false);
-  const [failMessage, setFailMessage] = useState("");
+  const [failMessage, setFailMessage] = useState('');
   // pagination
   const [currentPage, setCurrentPage] = useState(1);
   const postPerPage = 5;
@@ -23,15 +23,20 @@ const Dashboard = () => {
   const indexFirstPost = indexLastPost - postPerPage;
   const currentData = productData?.slice(indexFirstPost, indexLastPost);
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
+  const [isLoading, setIsLoading] = useState(true);
 
   const getData = async (token) => {
     await adminProduksi
-      .get("/orders", {
+      .get('/orders', {
         headers: {
-          "x-access-token": `${token}`,
+          'x-access-token': `${token}`,
         },
       })
-      .then((response) => setProductData(response.data));
+      .then((response) => {
+        setProductData(response.data);
+        setIsLoading(false);
+      })
+      .catch(() => setIsLoading(false));
   };
 
   const closeModal = () => {
@@ -43,7 +48,7 @@ const Dashboard = () => {
     await adminProduksi
       .get(`/orders/${id}`, {
         headers: {
-          "x-access-token": `${parseUser.data.token}`,
+          'x-access-token': `${parseUser.data.token}`,
         },
       })
       .then((response) => setDetailData(response.data));
@@ -58,7 +63,7 @@ const Dashboard = () => {
         },
         {
           headers: {
-            "x-access-token": `${parseUser.data.token}`,
+            'x-access-token': `${parseUser.data.token}`,
           },
         }
       )
@@ -80,7 +85,7 @@ const Dashboard = () => {
         },
         {
           headers: {
-            "x-access-token": `${parseUser.data.token}`,
+            'x-access-token': `${parseUser.data.token}`,
           },
         }
       )
@@ -102,7 +107,7 @@ const Dashboard = () => {
         },
         {
           headers: {
-            "x-access-token": `${parseUser.data.token}`,
+            'x-access-token': `${parseUser.data.token}`,
           },
         }
       )
@@ -221,65 +226,96 @@ const Dashboard = () => {
                 </tr>
               </thead>
               <tbody>
-                {currentData?.map((item, index) => (
-                  <tr className="border-b" key={index}>
-                    <td className="text-center p-3">{index + 1}</td>
-                    <td className="text-center p-3">{item.order_code}</td>
-                    <td className="text-center p-3">{`${new Date(
-                      item.createdAt
-                    ).getDate()} - ${
-                      new Date(item.createdAt).getMonth() + 1
-                    } - ${new Date(item.createdAt).getFullYear()}`}</td>
-                    <td className="text-left p-3">{item.users.user_ikm}</td>
-                    <td className="text-center p-3">
-                      <div className="relative">
-                        <select
-                          id="status"
-                          name="status"
-                          defaultValue={item.order_status}
-                          // value={item.status}
-                          onChange={(e) => handleChange(e, item)}
-                          className={`${
-                            item.order_status === null
-                              ? "!bg-gradient-to-bl !from-orange-900 !to-primary-900 hover:!from-primary-900 hover:!to-orange-900 !shadow-red"
-                              : item.order_status === 2
-                              ? "!bg-primary-400 hover:!bg-primary-400/80"
-                              : item.order_status === 3
-                              ? "!bg-secondary-800 hover:!bg-secondary-800/80"
-                              : item.order_status === 4
-                              ? "!bg-green-500 hover:!bg-green-500/80"
-                              : ""
-                          } input-field-select-xs !border-none !font-semibold !text-white !w-auto !pr-12`}
-                        >
-                          <option value={item.order_status}>
-                            {item.order_status === null
-                              ? "Status Produksi"
-                              : item.order_status === 2
-                              ? "Belum Produksi"
-                              : item.order_status === 3
-                              ? "Dalam Proses"
-                              : "Selesai Produksi"}
-                          </option>
-                          <option value="1">Status Produksi</option>
-                          <option value="2">Belum Diproduksi</option>
-                          <option value="3">Dalam Proses</option>
-                          <option value="4">Selesai Diproduksi</option>
-                        </select>
-                        <IoIosArrowDown className="absolute right-4 top-[15px] text-base fill-white" />
-                      </div>
-                    </td>
-                    <td>
-                      <div className="w-full flex justify-center">
-                        <button
-                          onClick={() => detailModalHandling(item.order_id)}
-                          className="bg-white border py-2 px-4 rounded-lg text-sm transition-200 hover:border-orange-900"
-                        >
-                          Detail
-                        </button>
-                      </div>
-                    </td>
-                  </tr>
-                ))}
+                {isLoading
+                  ? [1, 2, 3].map((item) => (
+                      <tr
+                        className="animate-pulse border-b"
+                        key={item}
+                      >
+                        <td className="text-center py-3 px-4">
+                          <div className="h-3 bg-slate-200 rounded-md"></div>
+                        </td>
+                        <td className="text-center py-3 px-4">
+                          <div className="h-3 bg-slate-200 rounded-md"></div>
+                        </td>
+                        <td className="text-center py-3 px-4">
+                          <div className="h-3 bg-slate-200 rounded-md"></div>
+                        </td>
+                        <td className="text-left py-3 px-4">
+                          <div className="h-3 bg-slate-200 rounded-md"></div>
+                        </td>
+                        <td className="text-center py-3 px-4">
+                          <div className="h-3 bg-slate-200 rounded-md"></div>
+                        </td>
+                        <td className="py-3 px-4">
+                          <div className="py-2 w-full flex justify-center">
+                            <div className="h-3 w-16 bg-slate-200 rounded-md"></div>
+                          </div>
+                        </td>
+                      </tr>
+                    ))
+                  : currentData?.map((item, index) => (
+                      <tr
+                        className="border-b"
+                        key={index}
+                      >
+                        <td className="text-center p-3">{index + 1}</td>
+                        <td className="text-center p-3">{item.order_code}</td>
+                        <td className="text-center p-3">{`${new Date(
+                          item.createdAt
+                        ).getDate()} - ${
+                          new Date(item.createdAt).getMonth() + 1
+                        } - ${new Date(item.createdAt).getFullYear()}`}</td>
+                        <td className="text-left p-3">{item.users.user_ikm}</td>
+                        <td className="text-center p-3">
+                          <div className="relative">
+                            <select
+                              id="status"
+                              name="status"
+                              defaultValue={item.order_status}
+                              // value={item.status}
+                              onChange={(e) => handleChange(e, item)}
+                              className={`${
+                                item.order_status === null
+                                  ? '!bg-gradient-to-bl !from-orange-900 !to-primary-900 hover:!from-primary-900 hover:!to-orange-900 !shadow-red'
+                                  : item.order_status === 2
+                                  ? '!bg-primary-400 hover:!bg-primary-400/80'
+                                  : item.order_status === 3
+                                  ? '!bg-secondary-800 hover:!bg-secondary-800/80'
+                                  : item.order_status === 4
+                                  ? '!bg-green-500 hover:!bg-green-500/80'
+                                  : ''
+                              } input-field-select-xs !border-none !font-semibold !text-white !w-auto !pr-12`}
+                            >
+                              <option value={item.order_status}>
+                                {item.order_status === null
+                                  ? 'Status Produksi'
+                                  : item.order_status === 2
+                                  ? 'Belum Produksi'
+                                  : item.order_status === 3
+                                  ? 'Dalam Proses'
+                                  : 'Selesai Produksi'}
+                              </option>
+                              <option value="1">Status Produksi</option>
+                              <option value="2">Belum Diproduksi</option>
+                              <option value="3">Dalam Proses</option>
+                              <option value="4">Selesai Diproduksi</option>
+                            </select>
+                            <IoIosArrowDown className="absolute right-4 top-[15px] text-base fill-white" />
+                          </div>
+                        </td>
+                        <td>
+                          <div className="w-full flex justify-center">
+                            <button
+                              onClick={() => detailModalHandling(item.order_id)}
+                              className="bg-white border py-2 px-4 rounded-lg text-sm transition-200 hover:border-orange-900"
+                            >
+                              Detail
+                            </button>
+                          </div>
+                        </td>
+                      </tr>
+                    ))}
               </tbody>
             </table>
           </div>
